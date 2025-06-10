d0b066c0369283b90e4f5e2811fa406628261dc2
Marlo-Byte <marlobyte.coder@gmail.com>
2025-06-10 19:09:08 -0300

    main

--- projects/script.js
+++ projects/script.js
@@ -1,4 +1,5 @@
-const BASE_PATH = "https://marlo-byte.github.io/resumev1";
+const BASE_PATH = window.location.hostname === "localhost" ? "" : "/resumev1";
+
 
 $(document).ready(function () {
     $('#menu').click(function () {
@@ -36,22 +37,26 @@ function getProjects() {
 }
 
 function showProjects(projects) {
-    let projectsContainer = document.querySelector(".work .box-container");
+    const projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
 
     projects.forEach(project => {
-        let viewBtn = project.links.view && project.links.view.trim() !== ""
+        const imgSrc = `${BASE_PATH}/assets/images/projects/${project.image}.png`;
+
+        // Botones condicionales
+        const viewBtn = project.links.view
             ? `<a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> Ver</a>`
             : "";
 
-        let codeBtn = project.links.code && project.links.code.trim() !== ""
+        const codeBtn = project.links.code
             ? `<a href="${project.links.code}" class="btn" target="_blank">Código <i class="fas fa-code"></i></a>`
             : "";
 
+        // Template HTML
         projectsHTML += `
         <div class="grid-item ${project.category}">
             <div class="box tilt" style="width: 380px; margin: 1rem">
-                <img draggable="false" src="${BASE_PATH}/assets/images/projects/${project.image}.png" alt="project" />
+                <img src="${imgSrc}" alt="project" onerror="this.onerror=null; this.src='https://via.placeholder.com/300x200.png?text=Imagen+no+disponible'; console.error('Imagen no encontrada:', '${imgSrc}')">
                 <div class="content">
                     <div class="tag">
                         <h3>${project.name}</h3>
@@ -70,23 +75,20 @@ function showProjects(projects) {
 
     projectsContainer.innerHTML = projectsHTML;
 
-    // isotope filter products
-    var $grid = $('.box-container').isotope({
+    // Inicializar Isotope
+    const $grid = $('.box-container').isotope({
         itemSelector: '.grid-item',
-        layoutMode: 'fitRows',
-        masonry: {
-            columnWidth: 200
-        }
+        layoutMode: 'fitRows'
     });
 
-    // filter items on button click
     $('.button-group').on('click', 'button', function () {
         $('.button-group').find('.is-checked').removeClass('is-checked');
         $(this).addClass('is-checked');
-        var filterValue = $(this).attr('data-filter');
+        const filterValue = $(this).attr('data-filter');
         $grid.isotope({ filter: filterValue });
     });
 }
+
 
 getProjects().then(data => {
     showProjects(data);
