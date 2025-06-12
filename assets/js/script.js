682e59092a4bcd888391503875ece54839d7459a
Marlo-Byte <marlobyte.coder@gmail.com>
2025-06-12 15:47:27 -0300

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-const BASE_PATH = "../";
-
 $(document).ready(function () {
 
     $('#menu').click(function () {
@@ -176,7 +174,7 @@ function showProjects(projects) {
 
             projectHTML += `
         <div class="box tilt">
-          <img draggable="false" src="${BASE_PATH}/assets/images/projects/${project.image}.png" alt="project" />
+          <img draggable="false" src="../assets/images/projects/${project.image}.png" alt="project" />
           <div class="content">
             <div class="tag">
               <h3>${project.name}</h3>
